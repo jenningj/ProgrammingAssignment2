8dc77eb62fee26cb8f2a137db55cba6814adb766
jenningj <jenny.j@me.com>
2015-03-17 18:28:55 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 ## function creates a special "matrix" object
 
 makeCacheMatrix <- function(x = matrix()) {
